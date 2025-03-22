ad644274f1820b9e992eaddc9ba7bcd8a075b363
Jose Daza <jose.daza@outlook.com>
2025-03-22 07:51:09 -0400

    Agregando stylos y otros fixes

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import bcrypt from "bcryptjs";
+import "../styles/Login.css";
 
 const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3000/api/";
 const LOGIN_URL = `${BACKEND_URL}auth/login`;
@@ -39,25 +40,30 @@ const Login = ({ setToken }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Email</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button type="submit">Login</button>
-    </form>
+    <div className="login-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <h2>Login</h2>
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="login-button">
+          Login
+        </button>
+      </form>
+    </div>
   );
 };
 
--- frontend/src/components/Tasks.js
+++ frontend/src/components/Tasks.js
@@ -2,33 +2,38 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import "../styles/Tasks.css";
 
 const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3000/api/";
 const TASKS_URL = `${BACKEND_URL}tasks`;
 
 const Tasks = ({ token }) => {
   const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState("");
   const [status, setStatus] = useState("");
   const [search, setSearch] = useState("");
+  const [newTask, setNewTask] = useState({
+    Titulo: "",
+    Descripcion: "",
+    Estado: "pendiente",
+    FechaLimite: new Date(),
+  });
+
+  const getTasks = async () => {
+    try {
+      const response = await axios.get(TASKS_URL, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: { status, search },
+      });
+
+      setTasks(response.data);
+    } catch (error) {
+      console.error(`ERROR: Getting the tasks: ${error}`);
+    }
+  };
 
   useEffect(() => {
-    const getTasks = async () => {
-      try {
-        const response = await axios.get(TASKS_URL, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          params: { status, search },
-        });
-
-        console.log(response.data);
-        setTasks(response.data);
-      } catch (error) {
-        console.error(`ERROR: Getting the tasks: ${error}`);
-      }
-    };
-
     getTasks();
   }, [token, status, search]);
 
@@ -37,10 +42,10 @@ const Tasks = ({ token }) => {
       await axios.put(
         `${TASKS_URL}/${task.id}`,
         {
-          Titulo: task.Titulo,
-          Descripcion: task.description,
-          Estado: task.Estado,
-          FechaLimite: task.FechaLimite,
+          title: task.Titulo,
+          description: task.Descripcion,
+          status: task.Estado,
+          dueDate: task.FechaLimite,
         },
         {
           headers: {
@@ -61,20 +66,66 @@ const Tasks = ({ token }) => {
         headers: { Authorization: `Bearer ${token}` },
       });
 
-      setTasks(tasks.filter((task) => task.id !== id));
+      // setTasks(tasks.filter((task) => task.id !== id));
+      getTasks();
     } catch (error) {
       console.error(`ERROR: Error eliminando la tarea ${id}: ${error}`);
     }
   };
 
+  const handleAgregar = async () => {
+    try {
+      const response = await axios.post(
+        TASKS_URL,
+        {
+          title: newTask.Titulo,
+          description: newTask.Descripcion,
+          status: newTask.Estado,
+          dueDate: newTask.FechaLimite.toISOString(),
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      setTasks([...tasks, response.data]);
+      setNewTask({
+        Titulo: "",
+        Descripcion: "",
+        Estado: "pendiente",
+        FechaLimite: new Date(),
+      });
+      alert("Tarea agregada.");
+    } catch (error) {
+      console.error(`ERROR: Error agregando la tarea: ${error}`);
+    }
+  };
+
+  const handleNewTaskChange = (field, value) => {
+    setNewTask((prevTask) => ({
+      ...prevTask,
+      [field]: value,
+    }));
+  };
+
+  const handleTaskChange = (id, field, value) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id ? { ...task, [field]: value } : task
+      )
+    );
+  };
+
   return (
-    <div>
-      <div>
+    <div className="tasks-container">
+      <div className="filter-container">
         <label>Estado: </label>
         <select value={status} onChange={(e) => setStatus(e.target.value)}>
           <option value="">Todos</option>
           <option value="pendiente">Pendiente</option>
-          <option value="en progreso">Pendiente</option>
+          <option value="en progreso">En Progreso</option>
           <option value="completada">Completado</option>
         </select>
       </div>
@@ -88,28 +139,72 @@ const Tasks = ({ token }) => {
         />
       </div>
 
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <div className="taskCard">
-              <h3>{task.Titulo}</h3>
-              <div>
-                <select
-                  value={task.Estado}
-                  onChange={(e) => (task.Estado = e.target.value)}
-                >
-                  <option value="pendiente">Pendiente</option>
-                  <option value="en progreso">Pendiente</option>
-                  <option value="completada">Completado</option>
-                </select>
+      <div className="new-task-container">
+        <h3>Agregar Nueva Tarea</h3>
+        <input
+          type="text"
+          placeholder="Título"
+          value={newTask.Titulo}
+          onChange={(e) => handleNewTaskChange("Titulo", e.target.value)}
+        />
+        <textarea
+          placeholder="Descripción"
+          value={newTask.Descripcion}
+          onChange={(e) => handleNewTaskChange("Descripcion", e.target.value)}
+        />
+        <select
+          value={newTask.Estado}
+          onChange={(e) => handleNewTaskChange("Estado", e.target.value)}
+        >
+          <option value="pendiente">Pendiente</option>
+          <option value="en progreso">En Progreso</option>
+          <option value="completada">Completado</option>
+        </select>
+        <DatePicker
+          selected={newTask.FechaLimite}
+          onChange={(date) => handleNewTaskChange("FechaLimite", date)}
+        />
+        <button onClick={handleAgregar}>Agregar</button>
+      </div>
 
-                <DatePicker selected={new Date(task.FechaLimite)} />
-              </div>
-              <input type="text" value={task.Descripcion} />
-              <div>
-                <button onClick={() => handleSave(task)}>Guardar</button>
-                <button onClick={() => handleDelete(task)}>Eliminar</button>
-              </div>
+      <ul className="tasks-list">
+        {tasks.map((task) => (
+          <li key={task.id} className="task-card">
+            <input
+              type="text"
+              value={task.Titulo}
+              onChange={(e) =>
+                handleTaskChange(task.id, "Titulo", e.target.value)
+              }
+            />
+            <div>
+              <select
+                value={task.Estado}
+                onChange={(e) =>
+                  handleTaskChange(task.id, "Estado", e.target.value)
+                }
+              >
+                <option value="pendiente">Pendiente</option>
+                <option value="en progreso">En Progreso</option>
+                <option value="completada">Completado</option>
+              </select>
+
+              <DatePicker
+                selected={new Date(task.FechaLimite)}
+                onChange={(date) =>
+                  handleTaskChange(task.id, "FechaLimite", date)
+                }
+              />
+            </div>
+            <textarea
+              value={task.Descripcion}
+              onChange={(e) =>
+                handleTaskChange(task.id, "Descripcion", e.target.value)
+              }
+            />
+            <div>
+              <button onClick={() => handleSave(task)}>Guardar</button>
+              <button onClick={() => handleDelete(task.id)}>Eliminar</button>
             </div>
           </li>
         ))}
